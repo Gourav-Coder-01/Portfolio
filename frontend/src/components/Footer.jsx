cf3f3bb28527a4bfe6c8fdd85d48caf52eb28817
Gourav-Coder-01 <gourav.kr201@gmail.com>
2026-03-22 14:06:58 +0530

    modify footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import { FaWhatsapp } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-8 px-6">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
+      <div className="max-w-6xl mx-auto flex flex-row justify-between items-center gap-6">
         
         {/* Left side */}
         <div className="text-center md:text-left">
@@ -10,16 +12,9 @@ const Footer = () => {
         </div>
 
         {/* Right side: subscription */}
-        <div className="flex flex-col sm:flex-row gap-4 sm:gap-2.5 w-full md:w-auto justify-center md:justify-end">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="p-2 border rounded w-full sm:w-auto text-sm sm:text-base"
-          />
-          <button className="bg-linear-to-r from-blue-500 to-purple-500 text-white rounded cursor-pointer px-4 py-2 text-sm sm:text-base hover:scale-105 duration-200">
-            Subscribe
-          </button>
-        </div>
+        <a target="_blank" href="https://wa.me/917210734600">
+        <FaWhatsapp className="text-4xl max-sm:text-3xl text-green-500 bg-white rounded-4xl hover:scale-110 duration-200 hover:shadow-[0_0_10px_10px_green]" />
+        </a>
       </div>
     </footer>
   )
